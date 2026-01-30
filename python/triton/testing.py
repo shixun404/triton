2da412b2f81f976815b325a43c76ff453f4f7bb7
shixun404 <shixun404@gmail.com>
2026-01-30 02:07:17 +0000

    rcgrouped

--- python/triton/testing.py
+++ python/triton/testing.py
@@ -124,6 +124,7 @@ def do_bench_cudagraph(fn, rep=20, grad_to_none=None, quantiles=None, return_mod
         return _summarize_statistics(ret, quantiles, return_mode)
 
 
+
 def do_bench(fn, warmup=25, rep=100, grad_to_none=None, quantiles=None, return_mode="mean"):
     """
     Benchmark the runtime of the provided function. By default, return the median runtime of :code:`fn` along with
@@ -146,7 +147,7 @@ def do_bench(fn, warmup=25, rep=100, grad_to_none=None, quantiles=None, return_m
 
     di = runtime.driver.active.get_device_interface()
 
-    # fn()
+    fn()
     di.synchronize()
 
     cache = runtime.driver.active.get_empty_cache_for_benchmark()
@@ -155,47 +156,39 @@ def do_bench(fn, warmup=25, rep=100, grad_to_none=None, quantiles=None, return_m
     start_event = di.Event(enable_timing=True)
     end_event = di.Event(enable_timing=True)
     start_event.record()
-    for _ in range(0):
-        # runtime.driver.active.clear_cache(cache)
+    for _ in range(5):
+        runtime.driver.active.clear_cache(cache)
         fn()
     end_event.record()
     di.synchronize()
     estimate_ms = start_event.elapsed_time(end_event) / 5
-    # print(f"estimate_ms:{estimate_ms} ms")
+
     # compute number of warmup and repeat
-    n_warmup = 5
-    n_repeat = 20
-    
-    # n_warmup = max(1, int(warmup / estimate_ms))
-    # n_repeat = max(1, int(rep / estimate_ms))
-    
+    n_warmup = max(1, int(warmup / estimate_ms))
+    n_repeat = max(1, int(rep / estimate_ms))
     start_event = [di.Event(enable_timing=True) for i in range(n_repeat)]
     end_event = [di.Event(enable_timing=True) for i in range(n_repeat)]
     # Warm-up
     for _ in range(n_warmup):
         fn()
     # Benchmark
-    start_event[0].record()
     for i in range(n_repeat):
         # we don't want `fn` to accumulate gradient values
         # if it contains a backward pass. So we clear the
         # provided gradients
-        # if grad_to_none is not None:
-        #     for x in grad_to_none:
-        #         x.grad = None
-        # # we clear the L2 cache before each run
-        # runtime.driver.active.clear_cache(cache)
+        if grad_to_none is not None:
+            for x in grad_to_none:
+                x.grad = None
+        # we clear the L2 cache before each run
+        runtime.driver.active.clear_cache(cache)
         # record time of `fn`
-        # start_event[i].record()
+        start_event[i].record()
         fn()
-    end_event[0].record()
+        end_event[i].record()
     # Record clocks
     di.synchronize()
-    ms = start_event[0].elapsed_time(end_event[0]) / n_repeat
-    print(ms)
-    # times = [s.elapsed_time(e) for s, e in zip(start_event, end_event)]
-    # return _summarize_statistics(times, quantiles, return_mode)
-    return ms, 0, 0
+    times = [s.elapsed_time(e) for s, e in zip(start_event, end_event)]
+    return _summarize_statistics(times, quantiles, return_mode)
 
 
 def assert_close(x, y, atol=None, rtol=None, err_msg=''):
